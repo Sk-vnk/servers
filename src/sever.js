bc14afafd880c7d87608fef5d9d233184ca6bc36
Xavier Pashley <xavipashley@gmail.com>
2023-12-11 17:06:25 +0000

    deleted 5-7

--- src/sever.js
+++ src/sever.js
@@ -2,9 +2,7 @@ const express = require("express");
 
 const app = express();
 
-// http://localhost:5001/server
-app.use("/apples", express.static("apples"));
-app.use("/oranges", express.static("oranges"));
+
 
 app.listen(5001, () => {
     console.log("Server is listening on port 5001");
