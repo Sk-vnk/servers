16a83439d5110ef0d7f28f3421e74f4c15cdc54b
Xavier Pashley <xavipashley@gmail.com>
2023-12-12 16:23:17 +0000

    enhancement: add db connection

--- src/sever.js
+++ src/sever.js
@@ -1,17 +1,49 @@
+require("dotenv").config();
+const { configDotenv } = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
+
+const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
 
-const books = [];
+const connection = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connected to MongoDB on the cloud");
+};
 
-app.post("/book", (request, response) => {
-  books.push(request.body);
+connection();
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+  },
+  genre: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+
+app.post("/book", async (request, response) => {
+    const book = await Book.create({
+        title: request.body.title,
+        author: request.body.author,
+        genre: request.body.genre,
+    });
+
 
   const successResponse = {
     message: "book added",
-    books: books,
+    book: book,
   };
 
   response.send(successResponse);
@@ -29,12 +61,11 @@ app.get("/book", (request, response) => {
 });
 
 app.delete("/book", (request, response) => {
-    const index = books.findIndex((x) => x.title === request.body.title);
-    
+  const index = books.findIndex((x) => x.title === request.body.title);
 });
 
 app.put("/book");
 
-app.listen(5001, () => {
-  console.log("Server is listening on port 5001");
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
